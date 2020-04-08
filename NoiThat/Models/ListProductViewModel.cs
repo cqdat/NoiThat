d9645c0226480adca3057ec85e66815d0e7b0367
Tuan Bui <kingtuan8@gmail.com>
2020-04-08 11:39:57 +0700

    w

--- NoiThat/Models/ListProductViewModel.cs
+++ NoiThat/Models/ListProductViewModel.cs
@@ -29,6 +29,15 @@ namespace NoiThat.Models
         public string CategoryName { get; set; }
         public string CategoryLink { get; set; }
         public Product product { get; set; }
+        public List<ProductImage> listimage { get; set; }
+        public List<Product> relate { get; set; }
+        public List<Product> upsell { get; set; }
+    }
+
+    public class QuickViewModel
+    {
+        public Product product { get; set; }
+        public List<ProductImage> listimage { get; set; }
     }
 
 }
---
+++ NoiThat/Scripts/client.js
@@ -0,0 +1,44 @@
+﻿$(document).ready(function () {
+
+    $(document).on("click", ".loadquickview", function (event) {
+
+        var id = $(this).attr("productid");
+
+        //var roleid = $("#hdRoleID").val();
+
+        event.preventDefault();
+        event.stopPropagation();
+
+        $.ajax({
+            url: '/product/GetQuickView',
+            contentType: 'application/html; charset=utf-8',
+            data: { id: id },
+            type: 'GET',
+            dataType: 'html'
+            , success: function (data) {
+
+                $("#viewquick").html(data);
+
+                //$('.cloud-zoom, .cloud-zoom-gallery').CloudZoom();
+
+                
+
+            },
+            error: function (xhr, status) {
+                alert("Fail connect to system server. Please try again or check internet connection.");
+
+            },
+            complete: function (xhr, status) {
+
+                //$.loadScript('../js/cloudzoom.js');
+                //$.loadScript('../js/cloud-zoom.js');
+
+                $('#QuickViewModal').modal({ backdrop: 'static', keyboard: false });
+            }
+        });
+
+    });
+
+
+
+});
